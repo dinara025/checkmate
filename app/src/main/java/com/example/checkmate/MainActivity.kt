cea7dc786681586146dea203d310e2d5dedb27d7
Nimsi Dharmaratne <nimsidinara@gmail.com>
2024-05-12 19:32:11 +0530

    inser data and search function

--- app/src/main/java/com/example/checkmate/MainActivity.kt
+++ app/src/main/java/com/example/checkmate/MainActivity.kt
@@ -1,31 +1,38 @@
 package com.example.checkmate
 
 import TasksDatabaseHelper
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.checkmate.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
-    private lateinit var db:TasksDatabaseHelper
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: TasksDatabaseHelper
     private lateinit var tasksAdapter: TasksAdapter
+    private lateinit var searchView: SearchView
+    private lateinit var searchList: ArrayList<Task>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        db = TasksDatabaseHelper(this)
-        tasksAdapter = TasksAdapter(db.getAllTasks(),this)
-        
-        binding.tasksRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.tasksRecyclerView.adapter=tasksAdapter
 
-        binding.addButton.setOnClickListener{
-            val intent= Intent(this, AddTaskActivity::class.java)
+        db = TasksDatabaseHelper(this)
+        tasksAdapter = TasksAdapter(db.getAllTasks(), this)
+
+        binding.tasksRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.tasksRecyclerView.adapter = tasksAdapter
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
             startActivity(intent)
         }
     }
@@ -33,5 +40,43 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         tasksAdapter.refreshData(db.getAllTasks())
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.layout.task_item,menu)
+        val searchItem = menu.findItem(R.id.search)
+        if (searchItem != null) {
+            searchView = searchItem.actionView as SearchView
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    searchView.clearFocus()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText != null) {
+                        val searchText = newText.toLowerCase(Locale.getDefault())
+                        if (searchText.isNotEmpty()) {
+                            searchList.clear()
+                            db.getAllTasks().forEach {
+                                if (it.title.toLowerCase(Locale.getDefault()).contains(searchText)) {
+                                    searchList.add(it)
+                                }
+                            }
+                            tasksAdapter.refreshData(searchList)
+                        } else {
+                            tasksAdapter.refreshData(db.getAllTasks())
+                        }
+                    }
+                    return true
+                }
+            })
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
